c1b20af1e3143857d89d20ffdf5677894a77a73b
Mojeedkush <mkusimo90@gmail.com>
2019-12-25 17:39:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+$(function (){
+    $(window).on("load resize", function () {
+        $(".fs").css("height", window.innerHeight).css("width", window.innerWidth);
+    });
+
+    // $("#video-wallpaper").wallpaper({
+    //     source: {
+    //         poster: "Jellyfish.jpg",
+    //         mp4: "demo-video.mp4",
+    //         video: "//www.youtube.com/embed/VIDEO_ID"
+    //     }
+    // });
+
+    // add Bootstrap's scrollspy
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 150
+    });
+
+    // smooth scrolling
+    $('nav a, .down-button a, .up-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 60
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // initializing wow.js
+    new WOW().init();
+
+    //initializing nanogallery
+    $(document).ready(function () {
+        $("#projects").nanoGallery();
+    });
+}); 
