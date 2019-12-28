edf2d4e5030749b24c927fb4d1952fe4d5ac8d45
Mojeedkush <mkusimo90@gmail.com>
2019-12-28 03:37:38 +0100

    testing github ci

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ $(function (){
     // add Bootstrap's scrollspy
     $('body').scrollspy({
         target: '.navbar',
-        offset: 150
+        offset: 100
     });
 
     // smooth scrolling
     $('nav a, .down-button a, .up-button a').bind('click', function () {
         $('html, body').stop().animate({
-            scrollTop: $($(this).attr('href')).offset().top - 60
+            scrollTop: $($(this).attr('href')).offset().top - 6
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
