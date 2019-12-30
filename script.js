02f6c589df8ad2122e3f2bfced3612a3076321e0
Mojeedkush <mkusimo90@gmail.com>
2019-12-30 23:13:05 +0100

    Mobile view

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ $(function (){
     // add Bootstrap's scrollspy
     $('body').scrollspy({
         target: '.navbar',
-        offset: 100
+        offset: 150
     });
 
     // smooth scrolling
-    $('nav a, .down-button a, .up-button a').bind('click', function () {
+    $('nav a, .down-button a').bind('click', function () {
         $('html, body').stop().animate({
-            scrollTop: $($(this).attr('href')).offset().top - 6
+            scrollTop: $($(this).attr('href')).offset().top - 60
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
